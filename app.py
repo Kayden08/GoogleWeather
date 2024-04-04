5be8770479f0b3657efdf366fd704e1ebfc7600b
hancokr <hancokr@pedarecc.sa.edu.au>
2024-04-04 14:11:28 +1030

    Added 'import requests'

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def search():
-    return render_template("home.html")
-@app.route('/results', methods=["GET", "POST"])
-def results():
+def home():
     api_key = "b12703d2df47873eaf71613c74db92a2"
-    city = request.form.get('city')
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&appid=" + api_key
+    city = "Adelaide"
+    url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&appid=" + api_key
+    print(url)
+    response = requests.get(url)
+    data = response.json()
+    return render_template("home.html")
+
+
+
+
+    #'description': data['weather'][0]['description'],
+
+# forcast_list = response['list'[0]]
+# for forecast is forcast_list:
+#     timestamp
+
 
 if __name__ == '__main__':
     app.run(debug=True)
